41b8582c7a10b207564bd3611848cdbfab012b72
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-09-04 15:37:25 +0200

    remove `import React` from `Counter.test.tsx`

--- template/src/components/Counter.test.tsx
+++ template/src/components/Counter.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Provider } from 'react-redux';
 import { render, fireEvent } from '@testing-library/react-native';
 import { configureStore } from '@reduxjs/toolkit';
